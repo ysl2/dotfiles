104bc719fffd11dd614360df972fcc40dd9b2c98
ysl2 <songliyu97@gmail.com>
2026-03-05 11:37:02 +0800

    wm: make keyboard setup hotplug-aware and single-instance

--- .vocal/0/scripts/wm/autostart_x11.d/keyboard.sh
+++ .vocal/0/scripts/wm/autostart_x11.d/keyboard.sh
@@ -1,10 +1,40 @@
 #!/bin/sh
-case "$XDG_SESSION_TYPE" in
-    x11 | tty)
-    # Switch capslock and escape
-    setxkbmap -option caps:swapescape
-esac
-# Acclerate keyboard
-xset r rate 250 30
-# Disable beep sound
-xset b 0 0 0
+
+apply_keyboard_settings() {
+    case "$XDG_SESSION_TYPE" in
+        x11 | tty)
+            # Switch capslock and escape
+            setxkbmap -option caps:swapescape
+            ;;
+    esac
+
+    # Accelerate keyboard
+    xset r rate 250 30
+    # Disable beep sound
+    xset b 0 0 0
+}
+
+get_keyboard_ids() {
+    xinput list --short 2>/dev/null \
+        | awk -F'id=' '/slave[[:space:]]+keyboard/ {print $2}' \
+        | awk '{print $1}' \
+        | tr '\n' ' '
+}
+
+watch_keyboard_hotplug() {
+    apply_keyboard_settings
+    last_keyboards="$(get_keyboard_ids)"
+
+    while true; do
+        current_keyboards="$(get_keyboard_ids)"
+
+        if [ "$current_keyboards" != "$last_keyboards" ]; then
+            apply_keyboard_settings
+            last_keyboards="$current_keyboards"
+        fi
+
+        sleep 1
+    done
+}
+
+watch_keyboard_hotplug
