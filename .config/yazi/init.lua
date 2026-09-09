a38871f377ba3d8e00bdd0b18aae5a33330e138e
ysl2 <songliyu97@gmail.com>
2026-09-09 22:12:40 +0800

    yazi: update to latest config.

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,5 +1,5 @@
--- NOTE: Use `ya pack -i` to restore all plugins.
+-- NOTE: Use `ya pkg install` to restore all packages from package.toml.
 
--- NOTE: `ya pack -a dedukun/relative-motions`
+-- NOTE: `ya pkg add dedukun/relative-motions`
 -- Ref: https://github.com/dedukun/relative-motions.yazi?tab=readme-ov-file#configuration
 require("relative-motions"):setup({ show_numbers="relative_absolute", show_motion = true, enter_mode ="first" })
